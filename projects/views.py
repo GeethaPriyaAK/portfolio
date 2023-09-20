e05c726fd97e573e9058550e102a33309ed883a5
Geetha Priya <akgeethapriya2@gmail.com>
2023-09-20 20:44:34 +0530

    first commit

---
+++ projects/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from projects.models import Project
+# Create your views here.
+
+def project_index(request):
+  projects = Project.objects.all()
+  context = {
+    "projects":projects
+  }
+  return render(request,"projects/index.html", context)
+
+
+def project_detail(request,pk):
+  project = Project.objects.get(pk=pk)
+  context = {
+    "project":project
+  }
+  return render(request, "projects/detail.html", context)
+
+def project_detail_1(request,pk):
+  project = Project.objects.get(pk=pk)
+  context = {
+    "project":project
+  }
+  return render(request, "projects/detail.html", context)
+
